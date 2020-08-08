ffd1a84bf8d3fe6b0434de1f07bd2396f3ba8842
nguyenle2457 <s3638100@rmit.edu.vn>
2020-08-08 17:26:14 +0700

    adding test cases

--- src/main/java/test/MyTest.java
+++ src/main/java/test/MyTest.java
@@ -3,7 +3,7 @@ package test;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import model.Student;
+import model.Customer;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,31 +17,88 @@ import java.util.List;
 
 public class MyTest {
 
-    @Test
-    public void test1() throws IOException {
+	//test method identifier
+	@Test
+	public void test1() throws IOException {
 
-        try{
-	        URL url = new URL(TestConfig.URL+"students");
-	        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-	        String line = "";
-	        StringBuilder stringBuilder = new StringBuilder();
-	
-	        while((line = bufferedReader.readLine()) !=null){
-	            stringBuilder.append(line);
-	        }
-	
-	        Gson gson = new Gson();
-	        String json = stringBuilder.toString();
-	        List<Student> students = gson.fromJson(json, new TypeToken<List<Student>>(){}.getType());
-	        //String s = stringBuilder.toString();
-	        Assert.assertEquals(students.get(0).getStudentName(), "Thanh");
-	        
-	    } catch (MalformedURLException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-    }
-}
+		try{
+			URL url = new URL(TestConfig.URL+"customers");
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+			String line = "";
+			StringBuilder stringBuilder = new StringBuilder();
 
+			while((line = bufferedReader.readLine()) !=null){
+				stringBuilder.append(line);
+			}
+
+			Gson gson = new Gson();
+			String json = stringBuilder.toString();
+			List<Customer> customers = gson.fromJson(json, new TypeToken<List<Customer>>(){}.getType());
+			//String s = stringBuilder.toString();
+			//Matches the value of student 0 to Thanh
+			Assert.assertEquals(customers.get(0).getCustomerName(), "Nguyen Van A");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	//test method identifier
+	@Test
+	public void test2() throws IOException {
+
+		try{
+			URL url = new URL(TestConfig.URL+"customers");
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+			String line = "";
+			StringBuilder stringBuilder = new StringBuilder();
+
+			while((line = bufferedReader.readLine()) !=null){
+				stringBuilder.append(line);
+			}
+
+			Gson gson = new Gson();
+			String json = stringBuilder.toString();
+			List<Customer> customers = gson.fromJson(json, new TypeToken<List<Customer>>(){}.getType());
+			//String s = stringBuilder.toString();
+			//Matches the value of student 0 to Thanh
+			Assert.assertEquals(customers.get(0).getCustomerAddress(), "702 Nguyen Van Linh District 7, Ho Chi Minh City");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	//test method identifier
+	@Test
+	public void test3() throws IOException {
+
+		try{
+			URL url = new URL(TestConfig.URL+"customers");
+			HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+			String line = "";
+			StringBuilder stringBuilder = new StringBuilder();
+
+			while((line = bufferedReader.readLine()) !=null){
+				stringBuilder.append(line);
+			}
+
+			Gson gson = new Gson();
+			String json = stringBuilder.toString();
+			List<Customer> customers = gson.fromJson(json, new TypeToken<List<Customer>>(){}.getType());
+			//String s = stringBuilder.toString();
+			//Matches the value of student 0 to Thanh
+			Assert.assertEquals(customers.get(0).getCustomerPhone(), "0912345678");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
